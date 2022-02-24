355472157f50eeb0bc239c5797abfa7696b9a66b
a20041060 <a20041060@gmail.com>
2022-02-24 19:30:35 +0800

    make it betteerrr

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect(URI,{
 if(process.env.NODE_ENV==="production"){
     app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'build','index.html'))
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
     })
 }
 
