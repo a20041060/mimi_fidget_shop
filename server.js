30bca28f8b7abde4a949073de069ee75f2a57377
a20041060 <a20041060@gmail.com>
2022-02-24 19:28:37 +0800

    make it betteerrrrr

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ mongoose.connect(URI,{
 })
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static('mimishop_client/build'))
+    app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'mimishop_client','build','index.html'))
+        res.sendFile(path.join(__dirname,'build','index.html'))
     })
 }
 
